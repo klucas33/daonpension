cd7aa2c99b15573f204e27a426d0b44e581a2893
klucas33 <75414723+klucas33@users.noreply.github.com>
2021-03-17 12:50:02 +0900

    daon

--- reservation/js/reservation.js
+++ reservation/js/reservation.js
@@ -1,7 +1,25 @@
 $(function(){
+    var i = 0
+    $('.tab tr td span label').each(function(i,e){
+        $(this).on('click',function(){
+            $(this).toggleClass('active')
+
+        if($(this).hasClass('active')){
+            
+        }else{
+
+        }
+     })
+    })
+
     $("#datepicker").datepicker({
+        
     	language: 'ko',
         timepicker: true,
         timeFormat: 'hh:ii AA'
     }); 
+    
+    
+        
+    
 })
