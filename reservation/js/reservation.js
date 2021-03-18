0b41c411fcac3de9af58adcde61313ab470e6ee0
klucas33 <75414723+klucas33@users.noreply.github.com>
2021-03-18 10:48:03 +0900

    daon

--- reservation/js/reservation.js
+++ reservation/js/reservation.js
@@ -6,26 +6,27 @@ $(function(){
 
             $('.tab tr').each(function(){
                 if($(this).find('label').hasClass('active')){
-                    console.log()
                     
                     var total = $(this).find('.purple option').filter(':selected').val() * '90000';                    
                     $(this).find('td:last p').text(total+'원')
 
                     all = parseInt( $(this).find('td:last p').text() ) * $('.tab tr label.active').length;
                     $('.last .box span').text(all)
+
+                    $('.purple option:selected').val() * $(this).find('td:last p').val(total)
+
                 }else{
                     $(this).find('td:last p').text('0원')
+                    $('.last .box span').text('0') * $('.tab tr label.active').length;
                 }
 
-                if($('.tab tr td:nth-of-type(4) select option').val() > 2){
-                    
-                }
+          
+                               
             })
     })
         
     
     $('#datepicker').datepicker({
-        
     	language: 'ko',
         timepicker: true,
         timeFormat: 'hh:ii AA'
