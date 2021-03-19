fe8d547be32942c8429947fd27ae56fce4d3ee94
klucas33 <75414723+klucas33@users.noreply.github.com>
2021-03-19 15:59:13 +0900

    daon

--- only you 101/js/o_y_101.js
+++ only you 101/js/o_y_101.js
@@ -1,11 +1,4 @@
 $(function(){
-    var unavailableDates = [
-        {start:'2015-09-11', end:'2015-09-15'},
-        {start:'2015-09-15', end:'2015-09-23'},
-        {start:'2015-10-01', end:'2015-10-07'}
-    ];
-
-
-    $('#calendar').availabilityCalendar(unavailableDates);
+    
 
 })
