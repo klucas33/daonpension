c5ad03023b16c02bf3968e507fb3db6435fcefa0
klucas33 <75414723+klucas33@users.noreply.github.com>
2021-03-19 17:59:19 +0900

    daon

---
+++ bbq/js/bbq.js
@@ -0,0 +1,34 @@
+$(function(){
+    $(window).scroll( function(){
+        $('.rock').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+            
+        });
+
+        $('.pop').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+            
+        });
+
+        $('.group .hole img').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+            
+        });
+         
+    });
+})
